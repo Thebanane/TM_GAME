92e9d3e8b49a8533f4767db362afb7af4a679c8a
Thebanane <hethaaran.2007@gmail.com>
2024-03-29 09:51:11 +0100

    ...

--- Objects_folder/Player/Player.gd
+++ Objects_folder/Player/Player.gd
@@ -1,29 +1,38 @@
 extends CharacterBody2D
+
 #variable vitesse
 @export var speed = 480
+
 #variables dash
 var dashspeed
 var dashing = false
 var can_dash = true
 var dash_direction: Vector2 = Vector2.ZERO
+
 #variables qui détérminera le déplacement du joueur
 @export var target_velocity = Vector2.ZERO
+
 #variables saut et gravité
 @export var gravity = 35
 @export var jump_impulse = 200
 var can_double_jump: bool = false
 var can_jump : bool
-#variables wall slide
+
+#variables wall slide and jump
 @export var wall_gravity = 20
+var wall_jump_speed  = 1500
+var slide_delay = true
+
 #variables animations
 @export var is_jumping = false
 @export var is_turning = false
+
 #variable utile pour l'animation turn
 var compare: bool  = true
 
 	
 #fonction qui va donner la vitesse du dash et deplacement du joueur
-func dash(parameter_direction):
+func dash():
 	if dashing:
 		if dash_direction == Vector2(0,-1) or dash_direction == Vector2(0,1):
 			dashspeed = 500
@@ -33,14 +42,16 @@ func dash(parameter_direction):
 			dashspeed = 550
 		target_velocity = dash_direction * dashspeed
 		can_dash = false
-	elif parameter_direction.x > 0:
+		
+func movement(parameter_direction) : 		
+	if parameter_direction.x > 0:
 		$Sprites_folder/girl_animation_sprites.flip_h = true
-		target_velocity.x = parameter_direction.x * speed
 	elif parameter_direction.x < 0:
 		$Sprites_folder/girl_animation_sprites.flip_h = false
-		target_velocity.x = parameter_direction.x * speed
 	else:
 		target_velocity.x = 0
+	target_velocity.x = parameter_direction.x * speed
+	
 #permet d'avoir la direction au moment de l'appuie comme ça on une direction fixe	
 	if Input.is_action_just_pressed("dash") and can_dash and! parameter_direction == Vector2(0,0):
 		dashing = true
@@ -77,7 +88,7 @@ func jump():
 		can_double_jump = false
 		Global.double_jump_input_pressed = true	
 	else:
-		$Double_jump.start()
+		$delay.start()
 		target_velocity.y += gravity
 		target_velocity.y = min(target_velocity.y, 900)
 		is_turning = false
@@ -85,20 +96,35 @@ func jump():
 
 
 #fonction qui va permettre de faire le slide sur le mur
-func slide(parameter):
-	if is_on_wall() and not is_on_floor() :
+func slide_and_jump(parameter_delta):
+	var wall_opposite = get_wall_normal()
+	if is_on_wall() and Input.is_action_pressed("grab"):
+		print(wall_opposite)
 		can_double_jump = false
-		target_velocity.y += wall_gravity * parameter
+		target_velocity.y += wall_gravity * parameter_delta
 		target_velocity.y = min(target_velocity.y, 300)
-#		if Input.is_action_just_pressed("jump"):
-#			target_velocity.y = -jump_impulse
+	
+
+	else:
+		$delay.start()
+		if Input.is_action_just_pressed("jump") and slide_delay:			
+			can_double_jump = true
+			slide_delay = false
+
+			target_velocity.x = wall_opposite.x * wall_jump_speed
+				
+			
+			target_velocity.y = -jump_impulse 
+
+
+
 
 #fonction qui va gérer toutes les animations du joueur
 func animation(parameter_target_velocity):
 	if is_on_floor() and is_turning:
 		$AnimationPlayer.play("turn") 
 		$Sprites_folder/Turn.flip_h = not $Sprites_folder/girl_animation_sprites.flip_h
-	elif is_on_wall_only():
+	elif is_on_wall_only() and Input.is_action_pressed('grab'):
 		$AnimationPlayer.play("slide")
 		$Sprites_folder/slide.flip_h = $Sprites_folder/girl_animation_sprites.flip_h	
 		if $Sprites_folder/slide.flip_h :
@@ -122,9 +148,10 @@ func _process(delta):
 #direction sera un vecteur x et y avec comme valeur max = 1. Noramized donne l'angle mais prend comme valeur de x et y = 1 
 	var direction = Input.get_vector("left","right","up","down").normalized()
 #appelle de toutes les fonction permettant à' l'action du joueur
-	dash(direction)
+	dash()
+	movement(direction)
 	jump()
-	slide(delta)
+	slide_and_jump(delta)
 	animation(target_velocity)
 #velocity est la variable interne du joueur et prendra la valeur de target velocity permettant ainsi de bouger et de sauter
 	velocity = target_velocity
@@ -134,5 +161,8 @@ func _process(delta):
 func _on_dash_stop_timeout():
 	dashing = false
 
-func _on_double_jump_timeout():
+func _on_delay_timeout():
 	can_jump = false
+	slide_delay = false
+
+
