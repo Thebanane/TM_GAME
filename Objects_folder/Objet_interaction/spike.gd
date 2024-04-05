fdb86d46b83594a66d55a5ca7cf98e0a38c370cc
Thebanane <hethaaran.2007@gmail.com>
2024-04-05 17:49:49 +0200

    Revert "05.04.23"
    This reverts commit 239da15219e03e1d95bbd9a11d30d751127676db.

--- Objects_folder/Objet_interaction/spike.gd
+++ Objects_folder/Objet_interaction/spike.gd
@@ -1,14 +1,1 @@
-extends Node2D
 
-var player_here: bool 
-
-func death():
-	if player_here:
-		get_tree().reload_current_scene()
-		
-func _process(_delta):
-	death()
-
-
-func _on_area_2d_body_entered(_body):
-	player_here = true
