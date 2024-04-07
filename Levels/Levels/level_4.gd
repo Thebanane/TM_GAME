7b35b32c086204626f1a3a3dedaccdd6f1e9b818
Thebanane <hethaaran.2007@gmail.com>
2024-04-07 09:27:02 +0200

    07.04.24_1

--- Levels/Levels/level_4.gd
+++ Levels/Levels/level_4.gd
@@ -8,18 +8,14 @@ func _on_camera_change_body_entered(body):
 		var tween = create_tween()
 		tween.tween_property($Player/Camera2D, "zoom", Vector2(2,2), 0.5)
 
-
-func _on_camera_change_2_body_entered(body):
-	if body == $Player :
-		var tween = create_tween()
-		tween.tween_property($Player/Camera2D, "zoom", Vector2(3.325,3.325), 0.3)
-
 #Lorsque on reçoit le signal (voir code spike), on fait bouger la porte et on modifie la limite de gauche de la caméra.
 func _on_switch_switch_on():
 	var tween = create_tween()
 	var tween2 = create_tween()
-	tween.tween_property($Objects/Door, "position", Vector2(-906,37.66), 0.8)
-	tween2.tween_property($Player/Camera2D,"limit_left", -1060, 0.8)
+	var tween3 = create_tween()
+	tween.tween_property($Objects/Door, "position", Vector2(-907,-21), 0.8)
+	tween2.tween_property($Player/Camera2D,"limit_left", -1122.5, 0.8)
+	tween3.tween_property($Player/Camera2D,"limit_right", -502.5, 0.8)
 
 
 func _on_death_body_entered(body):
