c34aefe63f8fbd14e54c96e18aa6f2f0856b15c5
Thebanane <hethaaran.2007@gmail.com>
2024-06-21 10:00:12 +0200

    21.06.24

--- Levels/UI/pause_menu.gd
+++ Levels/UI/pause_menu.gd
@@ -1,20 +1,11 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("pause_menu") and get_tree().paused: 
 		resume()		
 	elif Input.is_action_just_pressed("pause_menu") and! get_tree().paused:
 		pause()
-		
-
-		
 	
 func pause(): 
 	get_tree().paused = true
@@ -44,11 +35,16 @@ func _on_restart_pressed():
 
 func _on_main_menu_pressed():
 	$front_screen.visible = true
-	var tween = create_tween()
-	tween.tween_property($front_screen,"color", Color(0,0,0), 2)
-	await get_tree().create_timer(2.5).timeout
-	get_tree().paused = false
-	Global.music_intro_position = 0
-	Global.music_game_position = 0 
-	$PanelContainer/VBoxContainer.visible = false
-	get_tree().change_scene_to_file("res://Levels/UI/main_menu.tscn")
+	$AnimationPlayer.play("transition")
+
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "transition" :
+#		get_tree().paused = false
+#		Global.music_intro_position = 0
+#		Global.music_game_position = 0 
+#		$PanelContainer/VBoxContainer.visible = false
+		var main_menu = load("res://Levels/UI/main_menu.tscn")
+		get_tree().change_scene_to_packed(main_menu)
